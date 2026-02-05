1a36e076baafa057d96f88b2993c892163fa04d8
Kofi Arhin <kofiarhin69@gmail.com>
2026-02-05 07:26:01 +0000

    fix: restore app startup on linux and handle missing db gracefully

--- core/init.php
+++ core/init.php
@@ -1,52 +1,62 @@
-<?php 
+<?php
 
-	session_start();
+session_start();
 
-	require_once "functions/functions.php";
+require_once __DIR__ . '/../functions/functions.php';
 
-	define('application', realpath('./'));
+define('application', realpath(__DIR__ . '/..'));
 
-	$paths = array(
-		application,
-		application.'\classes',
-		get_include_path()
-	);
+$paths = array(
+    application,
+    application . DIRECTORY_SEPARATOR . 'classes',
+    get_include_path()
+);
+
+set_include_path(implode(PATH_SEPARATOR, $paths));
+
+spl_autoload_register(function ($class) {
+    $classFile = $class . '.php';
+    $lowerClassFile = strtolower($class) . '.php';
+
+    $resolvedClass = stream_resolve_include_path($classFile);
+    if ($resolvedClass !== false) {
+        require_once $resolvedClass;
+        return;
+    }
+
+    $resolvedLowerClass = stream_resolve_include_path($lowerClassFile);
+    if ($resolvedLowerClass !== false) {
+        require_once $resolvedLowerClass;
+        return;
+    }
+
+    throw new RuntimeException("Unable to load class file for {$class}");
+});
+
+$GLOBALS['config'] = array(
+
+    'mysql' => array(
+
+        'host' => '127.0.0.1',
+        'dbname' => 'test',
+        'user' => 'root',
+        'password' => 'root'
+    ),
 
 
-	set_include_path(implode(PATH_SEPARATOR, $paths));
+    'session' => array(
 
-	spl_autoload_register(function($class){
+        'session_name' => 'user',
+        'token_name' => 'token'
 
-		require_once $class.".php";
-		
-	});
-
+    ),
 
 
-	$GLOBALS['config'] = array(
+    'cookie' => array(
 
-		'mysql' => array(
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800
 
-			'host' => '127.0.0.1',
-			'dbname' => 'test',
-			'user' => 'root', 
-			'password' =>'root'
-		),
+    )
 
-
-		'session' => array(
-
-			'session_name' => 'user',
-			'token_name' => 'token'
-
-		),
-
-
-		'cookie' => array(
-
-			'cookie_name' => 'hash',
-			'cookie_expiry' => 604800
-
-		)
-
-	);
+);
